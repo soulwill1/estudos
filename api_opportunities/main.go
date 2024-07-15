f7c4066a3df8c2f55b644a24acfc663141d9b062
soulwill1 <willianverissimo88@gmail.com>
2024-07-15 17:20:58 -0300

    feat: add opening schema and custom logger

--- api_opportunities/main.go
+++ api_opportunities/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/soulwill1/estudos/router"
+import (
+	"github.com/soulwill1/estudos/config"
+	"github.com/soulwill1/estudos/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+
+	logger = *config.GetLogger("main")
+
+	// Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+
+	// Initialize router
 	router.Initialize()
 }
