1558fd57003709e7b9caa434c0f3bbafddda1840
soulwill1 <willianverissimo88@gmail.com>
2024-07-15 16:44:40 -0300

    feat: update handler structure to separate package

---
+++ api_opportunities/handler/handler.go
@@ -0,0 +1,35 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg":"POST Opening",
+	})
+}
+
+func ShowOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg":"GET Opening",
+	})
+}
+
+func DeleteOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg":"DELETE Opening",
+	})
+}
+func UpdateOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg":"PUT Opening",
+	})
+}
+func ListOpeningsHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg":"GET Openings",
+	})
+}
--- api_opportunities/router/routes.go
+++ api_opportunities/router/routes.go
@@ -1,9 +1,8 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/soulwill1/estudos/handler"
 )
 
 func initializeRoutes(router *gin.Engine) {
@@ -11,33 +10,10 @@ func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 
 	{
-		// Show opening
-		v1.GET("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg":"GET Opening",
-			})
-		})
-		v1.POST("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg":"POST Opening",
-			})
-		})
-		v1.DELETE("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg":"DELETE Opening",
-			})
-		})
-		v1.PUT("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg":"PUT Opening",
-			})
-		})
-		v1.GET("/openings", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg":"GET Openings",
-			})
-		})
+		v1.GET("/opening", handler.ShowOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
-	
-
 }
